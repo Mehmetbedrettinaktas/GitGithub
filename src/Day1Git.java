8c20d6bf90f6abf35fdd6c6dda06211b584147ee
Mehmet Bedrettin Aktas <mehmetbedrettinaktas@gmail.com>
2022-08-15 00:36:53 +0200

    yeni Class

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,5 +2,6 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println( "Hallo Git 1. Deneme");
         System.out.println( "Hallo Git 2. Deneme");
+        System.out.println( "Hallo Git 3. Deneme");
     }
 }
