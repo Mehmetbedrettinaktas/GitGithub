351719ce968454a1a2d47272bd78bd9f0a86dd67
Mehmet Bedrettin Aktas <mehmetbedrettinaktas@gmail.com>
2022-08-15 20:02:49 +0200

    V2

---
+++ src/day2/day2GitGithub.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class day2GitGithub {
+
+    /*
+    1) git iniz --> Local repo olusturmak icin yani .git ile klasorumuzun
+    icindeki dosyalari iliskilendirmek icin kullanilir
+    2) git add .  --> Wrking space'den (yani yerel) dosyalarimi staging area'ya
+    (yani commitlemek icin beklenen yer) gonderilir
+    3) git status --> Working space'deki ve staging area 'daki durumu gosterir
+    4) git commit -m"mesaj" ---> staging area dan commt storra dosyalarimi gondermek icin kullanilir
+    ( commit yani versiyon yani surum olusturmus olurum)
+    5) gir push ---> uzak repo'ya(yani remote - GitHub) gondermek icin kullandigimiz kod
+    Yalniz git push  komutunu kullanmak istersek 1 kez
+               git remote add origin  adress(github)taki adresimiz-
+               git pus -u origin master (bizim brancimizin adi)
+      Not: Yukardaki  iki komutu tek seferde kullandiktan sonra ikinci commit'lerim icin sadece
+      git push kullaniriz
+     */
+}
