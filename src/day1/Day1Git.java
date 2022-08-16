79f981d44e4908814e13da4a3a5c6e56759a3281
Mehmet Bedrettin Aktas <mehmetbedrettinaktas@gmail.com>
2022-08-16 14:04:57 +0200

    dayRepeat2 V1

--- src/day1/Day1Git.java
+++ src/day1/Day1Git.java
@@ -1,3 +1,5 @@
+package day1;
+
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println( "Hallo Git 1. Deneme");
--- src/day2/day2GitGithub.java
+++ src/day2/day2GitGithub.java
@@ -5,11 +5,15 @@ public class day2GitGithub {
     /*
     1) git init  --> Local repo olusturmak icin yani .git ile klasorumuzun
     icindeki dosyalari iliskilendirmek icin kullanilir
+
     2) git add .  --> Wrking space'den (yani yerel) dosyalarimi staging area'ya
     (yani commitlemek icin beklenen yer) gonderilir
+
     3) git status --> Working space'deki ve staging area 'daki durumu gosterir
+
     4) git commit -m"mesaj" ---> staging area dan commt storra dosyalarimi gondermek icin kullanilir
     ( commit yani versiyon yani surum olusturmus olurum)
+
     5) gir push ---> uzak repo'ya(yani remote - GitHub) gondermek icin kullandigimiz kod
     Yalniz git push  komutunu kullanmak istersek 1 kez
                git remote add origin  adress(github)taki adresimiz-
@@ -27,11 +31,9 @@ public class day2GitGithub {
 
       Not: Github ogrendim
       6) git pull ---> Remote(yani uzak repo) dosyalarimizi guncellemek icin kullanilir
-      
-<<<<<<< HEAD
-=======
->>>>>>> 5108b3d0a9ddc3021daf0f18b6ad47a1585e5846
->>>>>>> 7e3e56cb5e9a0858d0712fda30bb5792bffc8511
+
+      Not: git log --oneline --> Commitlerimdeki yani versionlarimdaki hash kodlarimin durumunu goruyorum
+
 
      */
     public static void main(String[] args) {
